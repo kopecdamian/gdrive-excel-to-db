60fb8d6b4dc378cfee965b5aa732fb53f37bb53f
root <root@ip-172-31-24-26.ec2.internal>
2025-11-27 23:44:50 +0000

    feat: Add save_to_db function [#2]

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
 import os
+import psycopg2
 
 load_dotenv()
 
@@ -17,8 +18,26 @@ def get_file():
     service = build('sheets', 'v4', credentials=creds)
     result = service.spreadsheets().values().get(
         spreadsheetId=SPREADSHEET_ID,
-        range="A:Z",
+        range="A2:Z",
     ).execute()
     print(result['values'])
 
-get_file()
+def save_to_db():
+    conn = psycopg2.connect(
+        host=os.getenv('PSQL_HOST'), 
+        port=5432,
+        dbname=os.getenv('DB_NAME'), 
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASS')
+    )
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO purchases (product, company, purchase_date) VALUES ('Kawa', 'Starbucks', '2025-01-12');")
+
+    conn.commit()
+            
+    cur.close()
+    conn.close()
+# get_file()
+
+save_to_db()
