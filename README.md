dae5e48970a2ca8aae1396c5e8e4f7b53847f873
Damian Kopeć <kopec.damian@icloud.com>
2025-11-30 16:32:38 +0000

    feat: insert data from Google Drive into database

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# get-receipts
+# gdrive-excel-to-db
+This program retrieves data from a spreadsheet using the Google Drive API and inserts it into a PostgreSQL table, ensuring no duplicate records are added.
 
-# Description
-This program get data from a  spreadsheet using Google Drive API.
 
 # Installation
 git clone https://github.com/kopecdamian/get-receipts.git
--- main.py
+++ main.py
@@ -9,20 +9,24 @@ load_dotenv()
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
 
+# Authorization for Google Drive
 def authenticate():
     creds = Credentials.from_service_account_file(os.getenv('SERVICE_ACCOUNT_FILE'), scopes = SCOPES)
     return creds
 
+# Get Excel file content from Google Drive
 def get_file():
     creds = authenticate()
     service = build('sheets', 'v4', credentials=creds)
     result = service.spreadsheets().values().get(
         spreadsheetId=SPREADSHEET_ID,
         range="A2:Z",
-    ).execute()
-    print(result['values'])
+    ).execute()['values']
+    return(result)
 
+# Save all retrieved data into the database table
 def save_to_db():
+    # open connection with database
     conn = psycopg2.connect(
         host=os.getenv('PSQL_HOST'), 
         port=5432,
@@ -31,13 +35,20 @@ def save_to_db():
         password=os.getenv('DB_PASS')
     )
     cur = conn.cursor()
+    
+    all_receipts = get_file()
 
-    cur.execute("INSERT INTO purchases (product, company, purchase_date) VALUES ('Kawa', 'Starbucks', '2025-01-12');")
+    # Insert only non-duplicate records into the database table
+    for receipt in all_receipts:
+        purchase_date = receipt[0]
+        product = receipt[1]
+        company = receipt[2]
+        cur.execute(f"INSERT INTO purchases (product, company, purchase_date) VALUES ('{product}', '{company}', '{purchase_date}') ON CONFLICT DO NOTHING;")
 
     conn.commit()
-            
+    
+    # close connection with database
     cur.close()
     conn.close()
-# get_file()
 
 save_to_db()
